e0e8993b183523ac89dc9754251c18a91133297f
Sit pra teste <joilson320@hotmail.com>
2019-05-14 00:43:23 -0300

    iniciando um projeto do github

---
+++ assets/function/validacao.js
@@ -0,0 +1,21 @@
+var usuarios = [
+    {"login": "teste@teste", "senha": "1234"},
+    {"login": "mamae", "senha": "abacaxi"},
+    {"login": "papai", "senha": "melancia"},
+];
+
+function Login() {
+    var usuario = document.getElementsByName('username')[0].value.toLowerCase();
+    var senha = document.getElementsByName('password')[0].value;
+
+    for (var u in usuarios) {
+        var us = usuarios[u];
+        if (us.login === usuario && us.senha === senha) {
+            window.location = "index.html";
+            return true;
+        }
+    }
+    alert("Dados incorretos, tente novamente.");
+    window.location = "index.html";
+    return false;
+}
